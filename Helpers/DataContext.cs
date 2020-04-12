147db7076b212fdb740d6939088cec14f8000359
tpereza <tpereza@unal.edu.co>
2020-04-12 10:53:31 -0500

    :tada::sparkles: Initial Commit

---
+++ Helpers/DataContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using WebApi.Entities;
+
+namespace WebApi.Helpers
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server database
+            options.UseMySql("server=localhost; port=3306; Database=authentication; Uid=root; Pwd=root");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
